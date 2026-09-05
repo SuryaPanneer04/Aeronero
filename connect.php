1cc222d5d48f7c4609b10cd858196c7f4508f93c
Vaithi <vaithi663@gmail.com>
2026-09-05 16:20:36 +0530

    myprofile ui added

--- connect.php
+++ connect.php
@@ -6,43 +6,40 @@ if (!headers_sent()) {
 
 //define("Title", 'Recruitment');
 try {
-	$con = new pdo ('mysql:host=localhost;dbname=demobluebase_bluebase','root',''); //admin@123
+    $con = new pdo('mysql:host=localhost;dbname=demobluebase_bluebase', 'root', ''); //admin@123
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
     $con->query("SET SESSION sql_mode = ''");
-} 
-catch (Exception $e)  
-{
-	echo $e->getMessage();
+} catch (Exception $e) {
+    echo $e->getMessage();
 }
 
 
 $IP = "http://192.168.200.92:8084";
 
-class Database{
-  
+class Database
+{
+
     // specify your own database credentials
     private $host = "localhost";
     private $db_name = "demobluebase_aeronero";
     private $username = "root";
-    private $password = ""; 
+    private $password = "";
     public $conn;
-  
+
     // get the database connection
-    public function getConnection(){
-  
-        $this->conn = null;  
-         try{
+    public function getConnection()
+    {
+
+        $this->conn = null;
+        try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
             $this->conn->query("SET SESSION sql_mode = ''");
             $this->conn->exec("set names utf8");
-        }
-        catch(PDOException $exception){
+        } catch (PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
-  
+
         return $this->conn;
     }
 }
-    
-?>
